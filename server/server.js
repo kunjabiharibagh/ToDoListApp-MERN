2fdd68720550a2406f047316b0fe3473df07b30c
kunjabiharibagh <kbbagh2005@gmail.com>
2026-03-15 15:07:24 +0530

    Fix deployment: add build script, serve React app, fix API URLs

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const connectDB = require("./config/db");
 const taskRoutes = require("./routes/taskRoutes");
 
@@ -12,8 +13,16 @@ app.use(express.json());
 
 app.use("/api", taskRoutes);
 
+// Serve static files from the React app build directory
+app.use(express.static(path.join(__dirname, "../client/build")));
+
 app.get("/", (req, res) => {
   res.send("Todo API Running");
+});
+
+// Catch all handler: send back React's index.html file for client-side routing
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
